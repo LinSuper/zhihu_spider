4ab2dfb914029d692fd8bba79d7f65a8f3dbb427
linchao <scut_linchao@163.com>
2016-01-23 15:34:36 +0800

    update model

---
+++ model/search_record.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from . import db
+from pymongo.collection import Collection
+
+
+class SearchRecord(object):
+    COL_NAME = 'search_record'
+    col = Collection(db, COL_NAME)
+    class Field(object):
+        _id = '_id'
+        url = 'url'
+        searchCount = 'searchCount'
+        title = 'title'
