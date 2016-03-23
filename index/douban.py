3fe89ccef79a03d6787aba8b1626ef92d490a762
linchao <linchao@jaxus.com>
2016-03-23 23:31:50 +0800

    豆瓣妹子增加分页

--- index/douban.py
+++ index/douban.py
@@ -8,10 +8,11 @@ from model.douban_topic import DoubanTopic
 
 @index.route('/douban', methods=['GET'])
 def douban_page():
-    page = request.args.get('page', 0, type=int)
+    page = request.args.get('page', 1, type=int)
     find_topics = DoubanTopic.col.find().sort(
         DoubanTopic.Field.create_time, -1
-    ).skip(page*20).limit(20)
+    ).skip((page-1)*20).limit(20)
+    count = DoubanTopic.col.count()
     data = []
     for topic in find_topics:
         temp = {}
@@ -20,4 +21,6 @@ def douban_page():
         temp['url'] = topic[DoubanTopic.Field.url]
         temp['author_url'] = topic[DoubanTopic.Field.author_url]
         data.append(temp)
-    return render_template('douban.html', index=5, data=data)
+    return render_template(
+        'douban.html', index=5, data=data, current_page=page, page_count=int((count+19)/20)
+    )
